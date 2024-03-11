3b4459551fa957c9e0380fe955a3d6dc7d5c546a
Marice Romero <marice.romero@gmail.com>
2024-03-11 15:07:53 -0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "aframe";
+import { Entity, Scene } from "aframe-react";
+
+function App() {
+  return (
+    <Scene embedded arjs="sourceType: webcam;">
+      <Entity
+        geometry={{ primitive: "box" }}
+        material={{ color: "red" }}
+        position={{ x: 0, y: 0, z: -5 }}
+        rotation={{ x: 0, y: 45, z: 0 }}
+      />
+      <Entity camera />
+    </Scene>
+  );
+}
+
+export default App;
