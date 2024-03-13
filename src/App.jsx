0c2ec3c880ead4cd19678c287964f62303d29581
Marice Romero <marice.romero@gmail.com>
2024-03-13 09:43:30 -0700

    Scaled bulbasaur model

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,11 @@ function App() {
         <a-asset-item id="bulbasaur" src={bulbasaur}></a-asset-item>
       </a-assets>
       {/* <Entity primitive="a-box" color="red" position="0 0 -5" /> */}
-      <Entity gltf-model="#bulbasaur" position={{ x: 0, y: 0, z: -2 }} />
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: 0, y: 0, z: -5 }}
+      />
     </Scene>
   );
 }
