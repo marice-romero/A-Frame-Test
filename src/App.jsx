2cd381ebcf6539dd3a40e8df4db1cd298e8a2211
Marice Romero <marice.romero@gmail.com>
2024-03-13 08:49:16 -0700

    Custom 3D model test

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,14 @@ import "./App.css";
 function App() {
   return (
     <Scene vr-mode-ui="enabled: false">
-      <Entity primitive="a-camera" position="0 1.6 0" />
-      <Entity primitive="a-box" color="red" position="0 0 -5" />
+      <a-assets>
+        <a-asset-item
+          id="astronaut"
+          src="https://cdn.glitch.com/ac5eecac-40b2-4897-8f67-28c497a19b47%2FAstronaut.glb"
+        ></a-asset-item>
+      </a-assets>
+      {/* <Entity primitive="a-box" color="red" position="0 0 -5" /> */}
+      <Entity gltf-model="#astronaut" position={{ x: 0, y: 0, z: -2 }} />
     </Scene>
   );
 }
