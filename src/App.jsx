d361b0880eacdf2e10a5cc89e3e6e447ade38aeb
Marice Romero <marice.romero@gmail.com>
2024-03-13 09:08:50 -0700

    Bulbasaur local file

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import "aframe";
 import "ar.js";
 import { Entity, Scene } from "aframe-react";
 import "./App.css";
+import bulbasaur from "./assets/bulbasaur.glb";
 
 function App() {
   return (
     <Scene vr-mode-ui="enabled: false">
       <a-assets>
-        <a-asset-item
-          id="astronaut"
-          src="https://cdn.glitch.com/ac5eecac-40b2-4897-8f67-28c497a19b47%2FAstronaut.glb"
-        ></a-asset-item>
+        <a-asset-item id="bulbasaur" src={bulbasaur}></a-asset-item>
       </a-assets>
       {/* <Entity primitive="a-box" color="red" position="0 0 -5" /> */}
-      <Entity gltf-model="#astronaut" position={{ x: 0, y: 0, z: -2 }} />
+      <Entity gltf-model="#bulbasaur" position={{ x: 0, y: 0, z: -2 }} />
     </Scene>
   );
 }
