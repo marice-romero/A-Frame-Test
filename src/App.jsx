f48ecf7339c2c5341f59af348acabe662539e48d
Marice Romero <marice.romero@gmail.com>
2024-03-13 19:51:06 -0700

    360 object tracking test 1

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,70 @@ function App() {
       <a-assets>
         <a-asset-item id="bulbasaur" src={bulbasaur}></a-asset-item>
       </a-assets>
-      {/* <Entity primitive="a-box" color="red" position="0 0 -5" /> */}
+      <Entity camera>
+        <Entity
+          gltf-model="#bulbasaur"
+          scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+          position={{ x: 0, y: 0, z: -5 }}
+        />
+      </Entity>
+
+      {/* <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: 1, y: 1, z: -2 }}
+      />
       <Entity
         gltf-model="#bulbasaur"
         scale={{ x: 0.75, y: 0.75, z: 0.75 }}
-        position={{ x: 0, y: 0, z: -5 }}
+        position={{ x: -1, y: 1, z: -3 }}
       />
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: 0, y: 3, z: -5 }}
+      />
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: 1, y: 3, z: -2 }}
+      />
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: -1, y: 3, z: -3 }}
+      />
+
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: 0, y: 0, z: -10 }}
+      />
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: 1, y: 1, z: -10 }}
+      />
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: -1, y: 1, z: -10 }}
+      />
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: 0, y: 3, z: -15 }}
+      />
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: 1, y: 3, z: -15 }}
+      />
+      <Entity
+        gltf-model="#bulbasaur"
+        scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+        position={{ x: -1, y: 3, z: -15 }}
+      /> */}
     </Scene>
   );
 }
